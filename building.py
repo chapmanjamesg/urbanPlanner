a8984383c6bde4bddf65701dde78d30c56b79efc
James Chapman <chapmanjamesg@gmail.com>
2020-01-16 12:00:51 -0600

    finished the exercise

--- building.py
+++ building.py
@@ -1,13 +1,24 @@
+import datetime
 class Building:
-    def __init__(self):
+    def __init__(self, address, stories):
         self.designer = "James Chapman"
         self.date_constructed = ""
         self.owner = ""
-        self.address = ""
-        self.stories = ""
-
-    def construct(self, owner, address, stories):
-        self.date_constructed = datetime.now()
-        self.owner = owner
         self.address = address
         self.stories = stories
+
+    def construct(self):
+        today = datetime.datetime.today()
+        self.date_constructed =  today.strftime("%m/%d/%Y")
+    
+    def purchase(self, owner):
+        self.owner = owner
+
+    def new_article(self):
+        print(f"{self.address} was purchased by {self.owner} on {self.date_constructed} and has {self.stories} stories.")
+
+
+three_hundred_fourth = Building("300 4th street", 20)
+three_hundred_fourth.purchase("Terry Crews")
+three_hundred_fourth.construct()
+three_hundred_fourth.new_article()
